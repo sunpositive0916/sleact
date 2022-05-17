fda26d5bc45faba95102ec8a9f5cf2c648649e2c
MOON <sunpositive7@gmail.com>
2022-05-17 21:00:54 +0900

    기본 설정 완료
    - 데브 서버 설정
    - 폴더 구조 설정
    - 리액트 라우터  배우기
    - 코드 스플리팅 배우기
    - 이모션 배우기

--- alecture/layouts/App.tsx
+++ alecture/layouts/App.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React from 'react';
+import loadable from '@loadable/component';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+// 코드스플리팅을 위해 loadable 사용(페이지 단위로 쪼개는것이 일반적)
+const LogIn = loadable(() => import('@pages/LogIn'));
+const SignUp = loadable(() => import('@pages/SignUp'));
 
 const App = () => {
-    return (
-        <div>테스트입니다~ 테스트~</div>
-    )
-}
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login" />
+      <Route path="/login" component={LogIn} />
+      <Route path="/signup" component={SignUp} />
+    </Switch>
+  );
+};
 
 export default App;

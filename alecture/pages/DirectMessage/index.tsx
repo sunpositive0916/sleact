94bef74204febf7dc857b39f024ee746cb05be50
MOON <sunpositive7@gmail.com>
2022-06-07 20:00:02 +0900

    DM페이지 만들기

--- alecture/pages/DirectMessage/index.tsx
+++ alecture/pages/DirectMessage/index.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import { Container, Header, DragOver } from '@pages/DirectMessage/styles';
+import fetcher from '@utils/fetcher';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import gravatar from 'gravatar';
+import { useParams } from 'react-router';
+import useSWR, { useSWRInfinite } from 'swr';
 
 const DirectMessage = () => {
-  return <div>DM이에요</div>;
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+  const { data: myData } = useSWR('/api/users', fetcher);
+
+  if (!userData || !myData) {
+    return null;
+  }
+
+  return (
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
+        <span>{userData.nickname}</span>
+      </Header>
+    </Container>
+  );
 };
 
 export default DirectMessage;
